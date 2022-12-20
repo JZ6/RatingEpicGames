f6b33fabe54307439dd3b7f57fa8b7d640f3d67b
JZ <yjzuo6@gmail.com>
2022-12-20 17:22:39 -0500

    DEC 20

--- src/data/update/update.mjs
+++ src/data/update/update.mjs
@@ -94,6 +94,6 @@ function addGame(name, dateString = new Date(), period = 7) {
 }
 
 // addGame('Saints Row IV', 'Thu Dec 08 2022')
-addGame("Them's Fightin' Herds", new Date(), 1)
+addGame("Wolfenstein: The New Order", new Date(), 1)
 update();
 
