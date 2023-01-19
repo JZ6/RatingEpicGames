78145e3c185f7b8fbdc61cee6f0fc05b50d04c39
JZ <yjzuo6@gmail.com>
2023-01-19 17:09:51 -0500

    Jan 19 update

--- src/node/update/index.mjs
+++ src/node/update/index.mjs
@@ -4,7 +4,7 @@ import importedGamesList from '../../data/freeGamesList.json' assert { type: "js
 async function update(params) {
     const modifiedGameList = { ...importedGamesList }
     // await addGame(modifiedGameList, `Shadow Tactics: Aiko's Choice`, new Date())
-    await addGame(modifiedGameList, `Epistory - Typing Chronicles`, 'January 19, 2023')
+    await addGame(modifiedGameList, `Adios`, 'January 26, 2023')
     // await addGame(modifiedGameList, 'Counter-Sadsatrike: Global Offensive')
 }
 
