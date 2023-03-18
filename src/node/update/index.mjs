bbff97fb338f0a6589196981140e24cbdd9991e7
JZ <yjzuo6@gmail.com>
2023-03-18 14:07:01 -0400

    Mar 18 update

--- src/node/update/index.mjs
+++ src/node/update/index.mjs
@@ -4,7 +4,7 @@ import importedGamesList from '../../data/freeGamesList.json' assert { type: "js
 async function update(params) {
     const modifiedGameList = { ...importedGamesList }
     // await addGame(modifiedGameList, `Shadow Tactics: Aiko's Choice`, new Date())
-    await addGame(modifiedGameList, `Dishonored: Death of the Outsider`, 'February 9, 2023')
+    await addGame(modifiedGameList, `Chess Ultra`, 'March 23, 2023')
     // await addGame(modifiedGameList, 'Counter-Sadsatrike: Global Offensive')
 }
 
