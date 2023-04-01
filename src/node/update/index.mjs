050dc11cce44abda327a78732028da73809bf1a4
JZ <yjzuo6@gmail.com>
2023-04-01 00:13:58 -0400

    March 30 update

--- src/node/update/index.mjs
+++ src/node/update/index.mjs
@@ -4,7 +4,7 @@ import importedGamesList from '../../data/freeGamesList.json' assert { type: "js
 async function update(params) {
     const modifiedGameList = { ...importedGamesList }
     // await addGame(modifiedGameList, `Shadow Tactics: Aiko's Choice`, new Date())
-    await addGame(modifiedGameList, `Warhammer 40,000: Gladius - Relics of War`, 'March 16, 2023')
+    await addGame(modifiedGameList, `Tunche`, 'March 30, 2023')
     // await addGame(modifiedGameList, 'Counter-Sadsatrike: Global Offensive')
 }
 
